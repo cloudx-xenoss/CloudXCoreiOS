fc6d8b2e7ee741bba26ba205c0561d847250c75c
Bryan Boyko <bryan.boyko@gmail.com>
2025-03-20 16:09:31 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "CloudXCore",
+    name: "CloudXCore",  // ✅ Matches framework name, NOT repo name
     platforms: [
         .iOS(.v14)
     ],
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CloudXCore",
-            url: "https://github.com/cloudx-xenoss/CloudXCoreiOS/releases/download/1.0.0/CloudXCore.xcframework.zip",
+            url: "https://github.com/cloudx-xenoss/CloudXCoreiOS/releases/latest/download/CloudXCore.xcframework.zip",
             checksum: "065564397d5191c99fd424abab9df5ec068406b8bafcbe122c646466cbe88a2f"
         )
     ]
