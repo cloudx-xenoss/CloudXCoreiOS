75ff8c45fd0fb703e2a09e17b2f4a0e7e2d31318
Bryan Boyko <bryan.boyko@gmail.com>
2025-03-20 16:00:13 -0700

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "CloudXCore",
+    platforms: [
+        .iOS(.v14)
+    ],
+    products: [
+        .library(
+            name: "CloudXCore",
+            targets: ["CloudXCore"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "CloudXCore",
+            url: "https://github.com/cloudx-xenoss/CloudXCoreiOS/releases/download/1.0.0/CloudXCore.xcframework.zip",
+            checksum: "065564397d5191c99fd424abab9df5ec068406b8bafcbe122c646466cbe88a2f"
+        )
+    ]
+)
