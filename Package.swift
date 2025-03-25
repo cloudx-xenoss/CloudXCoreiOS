e5c6cb9a794fd88c9c891775bc046cd6ac15b537
Bryan Boyko <bryan.boyko@gmail.com>
2025-03-25 14:38:55 -0700

    Update Package.swift to point to latest release

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CloudXCore",
-            url: "https://github.com/cloudx-xenoss/CloudXCoreiOS/releases/download/1.0.2/CloudXCore.xcframework.zip",
+            url: "https://github.com/cloudx-xenoss/CloudXCoreiOS/releases/latest/download/CloudXCore.xcframework.zip",
             checksum: "065564397d5191c99fd424abab9df5ec068406b8bafcbe122c646466cbe88a2f"
         )
     ]
